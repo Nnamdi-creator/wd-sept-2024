bf09a49adc8f06c840ae48876a6be42e6982ef3e
Nnamdi-creator <justiceurum@gmail.com>
2024-11-03 13:39:50 -0800

    Add files via upload

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,11 @@
-let age = prompt("enter your age")
-if(age > 18){
-    alert("Tax Eligibility")
-}
-else{
-    alert("Not Eligible")
-}
+let a = 6,
+    b = 5,
+    c = 8;
+let addScore = a + b + c
+let subScore = a - b - c
+let mulScore = a * b * c
+console.log(addScore,
+            subScore,
+            mulScore
+        );
+
