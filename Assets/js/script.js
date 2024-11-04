89a0a58e105af57433c29c14eb2374846315b6f1
Nnamdi-creator <justiceurum@gmail.com>
2024-11-04 15:34:56 -0800

    Add files via upload

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,22 @@
-let a = 6,
-    b = 5,
-    c = 8;
-let addScore = a + b + c
-let subScore = a - b - c
-let mulScore = a * b * c
-console.log(addScore,
-            subScore,
-            mulScore
-        );
+const score = [1,2,3,4,5,6,7,8,9,10,11];
+score.sort(function (x,y){
+    return x-y
+})
+console.log(score)
+score.sort(function (x,y){
+    return y-x
+})
+console.log(score)
+
+//middle element
+let midElement = Math.floor(score.lenght/2)
+//smallest element
+let smallestElement = Math.min(score)
+//largest element
+let largestElement = Math.max(score)
+console.log(
+     midElement,
+    smallestElement,
+    largestElement
+)  
 
